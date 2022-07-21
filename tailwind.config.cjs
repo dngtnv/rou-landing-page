5bbe5ba2672ec0b5e72b25e8d3f8a464eff5732f
dngtnv <kakalupin@gmail.com>
2022-07-21 13:13:10 +0700

    Navbar & header with hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#111111',
-        accent: '#9c59a4',
+        secondary: '#5e5858',
+        accent: '#c04ae2',
         light: {
           pink: '#fcd7df',
           green: '#fcd7df',
@@ -24,22 +25,23 @@ module.exports = {
           sm: '2rem',
           lg: '4rem',
           xl: '5rem',
-          '2xl': '6rem',
+          '2xl': '18.75rem',
         },
       },
       fontFamily: {
         sans: ['Overpass', 'sans-serif'],
       },
       fontSize: {
-        sm: ['clamp(1.00rem, calc(0.92rem + 0.39vw), 1.20rem)', '1.4'],
-        base: ['clamp(1.13rem, calc(0.98rem + 0.73vw), 1.50rem)', '1.5'],
-        lg: ['clamp(1.27rem, calc(1.03rem + 1.19vw), 1.88rem)', '1.4'],
+        sm: ['clamp(0.83rem, calc(0.82rem + 0.07vw), 0.89rem)', '1.4'],
+        base: ['clamp(1.00rem, calc(0.00rem + 0.00vw), 1.00rem)', '1.5'],
+        lg: ['clamp(1.09rem, calc(1.08rem + 0.07vw), 1.15rem);', '1.75'],
         xl: ['clamp(1.42rem, calc(1.06rem + 1.80vw), 2.34rem)', '1.4'],
         '2xl': ['clamp(1.60rem, calc(1.08rem + 2.59vw), 2.93rem)', '1.2'],
         '3xl': ['clamp(1.80rem, calc(1.08rem + 3.63vw), 3.66rem)', '1.1'],
         '4xl': ['clamp(2.03rem, calc(1.03rem + 4.98vw), 4.58rem)', '1'],
         '5xl': ['clamp(2.28rem, calc(0.94rem + 6.71vw), 5.72rem)', '1'],
         '6xl': ['clamp(2.57rem, calc(0.78rem + 8.95vw), 7.15rem)', '1'],
+        '7xl': ['clamp(3.27rem, calc(2.92rem + 1.73vw), 4.58rem)', '1'],
       },
     },
   },
